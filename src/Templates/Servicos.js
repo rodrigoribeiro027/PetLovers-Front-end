8f43e1b65d16918a728c663aaa928c363199f10d
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2023-06-10 19:50:36 -0300

    Adicionado seleção e compra de produto.

--- src/Templates/Servicos.js
+++ src/Templates/Servicos.js
@@ -1,111 +1,136 @@
 
-import { useEffect, useState,useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import {
     StyleSheet,
     Text,
     View,
-    TouchableOpacity, Image, ImageBackground,ScrollView
+    TouchableOpacity, Image, ImageBackground, ScrollView,
 } from 'react-native';
 import StatusBar from '../components/StatusBar.js';
 import * as Animatable from 'react-native-animatable'
 import stylesDefault from '../styles.js';
 import axios from 'axios';
 import { getStorageItem } from '../functions/encryptedStorageFunctions.js';
-import {useFocusEffect  } from '@react-navigation/native';
+import { useFocusEffect } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CriarServico = ({ navigation }) => {
 
     const [ofertas, setOferta] = useState([])
     const [tipo, setTipo] = useState('')
-
+    const [selecionado, setSelecionado] = useState([])
 
     const buscarOfertas = async () => {
         let url = 'https://pet-lovers-back-end.vercel.app/oferta/buscar';
-        if(tipo == 'Servicos'){
+        if (tipo === 'Servicos') {
             url = 'https://pet-lovers-back-end.vercel.app/oferta/buscarServicos';
         }
-        if(tipo == 'Produtos'){
+        if (tipo === 'Produtos') {
             url = 'https://pet-lovers-back-end.vercel.app/oferta/buscarProdutos';
         }
         const token = await getStorageItem('token');
-        axios.get(url, { headers: { Authorization: token } }).then((res) => {
-            setOferta(res.data);
-        }).catch(error => {
-            console.error('Erro', error.response);
-        })
+        axios
+            .get(url, { headers: { Authorization: token } })
+            .then((res) => {
+                const updatedOfertas = res.data.map((oferta) => ({
+                    ...oferta,
+                    selecionado: false,
+                }));
+                setOferta(updatedOfertas);
+            })
+            .catch((error) => {
+                console.error('Erro', error.response);
+            });
+    };
+    const toggleSelection = (index) => {
+        const updatedOfertas = [...ofertas];
+        updatedOfertas[index].selecionado = !updatedOfertas[index].selecionado;
+        console.log(updatedOfertas);
+        setOferta(updatedOfertas);
+    };
+    const realizarCompra = async () =>{
+        const token = await getStorageItem('token');
+        const selecionados = ofertas.filter(oferta=> oferta.selecionado).map(oferta=>oferta._id)
+        axios.post("https://pet-lovers-back-end.vercel.app/compra/cadastrar-multiplos",{ ofertas:selecionados },{ headers: { Authorization: token } })
+        .then((res)=>{
+            console.log('a compra foi realizada',res.data);
+            navigation.navigate("Home")
+        }) .catch(error => {
+            console.error('Erro', error);
+        });
     }
 
     useEffect(() => {
         buscarOfertas();
-    }, [tipo]); // Atualiza a lista de ofertas quando o valor de 'tipo' é alterado
+    }, [tipo]);
 
     useFocusEffect(
         useCallback(() => {
-            buscarOfertas(); // Atualiza a lista de ofertas quando a tela está em foco
+            buscarOfertas();
         }, [])
     );
 
     return (
         <>
             <ScrollView style={styles.screen}>
-
-            <View style={styles.containerCadastro}>
-                <StatusBar />
-                <TouchableOpacity style={stylesDefault.buttonVoltarDefault} onPress={() => navigation.navigate("Home")} >
-                    <Text style={stylesDefault.buttonTextDefault}>Voltar</Text>
-                </TouchableOpacity>
-                <Animatable.Text animation='fadeInRight' style={styles.TextPrincipaltextoCadastro}>Serviços</Animatable.Text>
-                <Animatable.View animation='fadeInRight' style={styles.divButton}>
-                    <View style={styles.options}>
-                        <View style={styles.optionBtn}>
-                            <View>
-                                <TouchableOpacity onPress={() => setTipo('Servicos')} >
-                                    <View style={{...styles.container, borderWidth: tipo === "Servicos" ?  1 : 0}}>
-                                        <ImageBackground source={require("../assets/servico.png")} style={{ width: 150, height: 115 }} >
-                                            <Text style={styles.text}>Serviços</Text>
-                                        </ImageBackground>
-                                    </View>
-                                </TouchableOpacity>
+                <View style={styles.containerCadastro}>
+                    <StatusBar />
+                    <Animatable.Text animation='fadeInRight' style={styles.TextPrincipaltextoCadastro}>Serviços</Animatable.Text>
+                    <Animatable.View animation='fadeInRight' style={styles.divButton}>
+                        <View style={styles.options}>
+                            <View style={styles.optionBtn}>
+                                <View>
+                                    <TouchableOpacity onPress={() => setTipo('Servicos')} >
+                                        <View style={{ ...styles.container, borderWidth: tipo === "Servicos" ? 1 : 0 }}>
+                                            <ImageBackground source={require("../assets/servico.png")} style={{ width: 150, height: 115 }} >
+                                                <Text style={styles.text}>Serviços</Text>
+                                            </ImageBackground>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <View style={styles.optionBtn}>
+                                <View>
+                                    <TouchableOpacity onPress={() => setTipo('Produtos')} >
+                                        <View style={{ ...styles.container, borderWidth: tipo === "Produtos" ? 1 : 0 }}>
+                                            <ImageBackground source={require("../assets/Product.png")} style={{ width: 150, height: 121 }} >
+                                                <Text style={styles.text}>Produtos</Text>
+                                            </ImageBackground>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
                         </View>
-                        <View style={styles.optionBtn}>
-                            <View>
-                                <TouchableOpacity onPress={() => setTipo('Produtos')} >
-                                    <View style={{...styles.container, borderWidth: tipo === "Produtos" ?  1 : 0}}>
-                                        <ImageBackground source={require("../assets/Product.png")} style={{ width: 150, height: 121 }} >
-                                            <Text style={styles.text}>Produtos</Text>
-                                        </ImageBackground>
-                                    </View>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-                </Animatable.View>
-                {ofertas.map((oferta) => (
-                            <View style={styles.ServicoConteiner} key={oferta._id}>
-                                <View style={styles.servicoText} >
+                    </Animatable.View>
+                    {ofertas.map((oferta, index) => (
+                        <View style={styles.ServicoConteiner} key={oferta._id}>
+                            <TouchableOpacity onPress={() => toggleSelection(index)} >
+                                <View style={styles.servicoText}>
                                     <Text style={styles.servicoTexto}>{oferta.nome}</Text>
                                     <Text style={styles.servicoTexto}>R$ {oferta.preco}</Text>
+                                    <Icon.Button name={oferta.selecionado? "check" : "square-o"} size={20} color="black" backgroundColor="#FFF"/>
                                 </View>
-                                <Text style={styles.servicoTexto}>
-                                    Descrição: {oferta.descricao}</Text>
-                            </View>
-                        ))}
-            </View>
+                            <Text style={styles.servicoTexto}>Descrição: {oferta.descricao}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                    <TouchableOpacity  onPress={() => realizarCompra()}>
+                            <Text>botao</Text>
+                        </TouchableOpacity>
+                </View>
             </ScrollView>
-
         </>
     )
 };
 const styles = StyleSheet.create({
     ServicoConteiner: {
-        width:'80%',
+        width: '80%',
         borderRadius: 5,
         padding: 5,
         fontSize: 10,
         backgroundColor: "#D3E5ED",
         marginBottom: 10,
-        padding:10
+        padding: 10
 
     },
     servicoText: {
